a22257bd0848dd1443f6bfdd3f3f6a5f6f14d701
quiquek007 <quiquek007@gmail.com>
2022-05-28 20:43:33 +0300

    feat: prettier + eslint

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 import express from 'express';
 import crypto from 'crypto';
-import Joi from 'joi'
+import Joi from 'joi';
 import { createValidator } from 'express-joi-validation';
 import getAutoSuggestUsers from './utils/suggest-fn.js';
 
 const app = express();
+// eslint-disable-next-line no-undef
 const port = Number(process.env.PORT) || 4200;
 const router = express.Router();
 const userStorage = [];
 const schema = Joi.object({
     login: Joi.string().required(),
-    password: Joi.string().alphanum().regex(/(?:\d)/).regex(/[a-z]/i).required(),
+    password: Joi.string()
+        .alphanum()
+        .regex(/(?:\d)/)
+        .regex(/[a-z]/i)
+        .required(),
     age: Joi.number().min(4).max(130).required()
 });
 const validator = createValidator();
 
 app.use(express.json());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     // Website you wish to allow to connect running front-end application on port 3000
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); 
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
@@ -28,7 +33,7 @@ app.use(function(req, res, next) {
     // Pass to next layer of middleware
     res.set('Cache-Control', 'no-store');
     next();
-})
+});
 router.get('/user', (req, res) => {
     res.status(404).json(null);
 });
@@ -43,7 +48,7 @@ router.delete('/user/:id', (req, res) => {
     const user = userStorage.find(u => u.id === req.params.id);
 
     if (!user) return res.status(404).json('fail');
-    
+
     user.isDeleted = true;
     res.status(200).json('success');
 });
@@ -73,7 +78,7 @@ router.put('/update-user', (req, res) => {
 
     if (!~userIdx) return res.status(400).json('fail');
 
-    userStorage[userIdx] = {...req.body};
+    userStorage[userIdx] = { ...req.body };
     res.status(200).json('success');
 });
 
