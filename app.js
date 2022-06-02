540f19110784a5f0c220c66bbf4ed73e4e97c7b2
quiquek007 <quiquek007@gmail.com>
2022-06-02 17:10:23 +0300

    feat: add realisation for 5.3

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import winston from 'winston';
 import SequelizeConnection from './sequelize-connection.js';
 import userRouter from './user-routes/controllers/user-controller.js';
 import groupRouter from './group-routes/controllers/group-controller.js';
@@ -10,6 +11,18 @@ new SequelizeConnection();
 const app = express();
 // eslint-disable-next-line no-undef
 const port = Number(process.env.PORT) || 4200;
+const logConfiguration = {
+    transports: [
+        new winston.transports.Console({
+            level: 'warn'
+        }),
+        new winston.transports.File({
+            level: 'error',
+            filename: 'logs/example.log'
+        })
+    ]
+};
+const logger = winston.createLogger(logConfiguration);
 
 app.use(express.json());
 app.use(function (req, res, next) {
@@ -30,19 +43,25 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
 
+app.use((req, res, next) => {
+    console.time('Execution time: ');
+    res.on('finish', () => console.timeEnd('Execution time: '));
+    next();
+});
+
 app.use('/', userRouter);
 app.use('/', groupRouter);
 app.use('/', userGroupRouter);
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
+    logger.error(err.stack);
     res.status(500).send('Something broke!');
     next(); // calling next middleware function or handler
 });
 
 // eslint-disable-next-line no-undef
 process.on('uncaughtException', err => {
-    console.error('There was an uncaught error', err);
+    logger.error('There was an uncaught error', err);
     // eslint-disable-next-line no-undef
     process.exit(1); // mandatory (as per the Node.js docs)
 });
